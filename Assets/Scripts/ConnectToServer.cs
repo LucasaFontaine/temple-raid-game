1812ab0dd6f25a194f3f3df28e130776425e46c3
LucasaFontaine <78568019+LucasaFontaine@users.noreply.github.com>
2026-03-17 18:36:43 +1300

    Revert "world change button"
    This reverts commit 1e379949f6e4d96eb430a1f1b1818154f96694dc.

--- Assets/Scripts/ConnectToServer.cs
+++ Assets/Scripts/ConnectToServer.cs
@@ -8,7 +8,6 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
 {
     void Start()
     {
-        PhotonNetwork.AutomaticallySyncScene = true;
         PhotonNetwork.ConnectUsingSettings();
     }
 
